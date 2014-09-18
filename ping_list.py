2181e99247d68384062d5e6de05b3f43e030b822
-ChenZongxiong <-czxczf@gmail.com>
2014-09-18 15:08:14 +0800

    Use regular expression -Use regular expression to confirm the ip user inputted whether is right or not. -Use regular expression to find out the ip in logger

--- ping_list.py
+++ ping_list.py
@@ -1,79 +1,59 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import time
 import sys
-
+import re
 
 #ip_prefix = "10.4.6."
 logger_path = "logger.txt"
+
 def os_ping(ip_prefix, lower_hostid, upper_hostid):
     if os.path.exists(logger_path):
         os.remove(logger_path)
+
     st = int(time.time())
 
     for i in range(int(lower_hostid), int(upper_hostid)):
         ip = ip_prefix + str(i)
         #TODO: ping cost too much time, should be optimized
-        res = os.system("ping -c 10 -W 1 "+ip+ " |tail -3 >> " + logger_path) 
-        if res:
-            print ip + " ping failed"
+        res = os.system("ping -c 10 -W 1 "+ip+ " |tail -3 >> " + logger_path)
+#    if res:
+#            print ip + " ping failed"
     ft = int(time.time())
     print "time:"+str(ft - st)+"s"
 
 def parse_logger():
     if not os.path.exists(logger_path):
         print "No ping logger exists, something must be wrong!"
-        return
-        
-    success = 0
-    loss  = 0
-    fail = 0
+        return False        
+    success = lost = fail = 0
+    #ip regular expression
     f = open(logger_path)
+    ip_pattern = '([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}'
+    num_pattern = '\d{1,3}\.\d%'
+    percent = ''
     while True:
         line = f.readline()
         if not line:
             break
-        
-        ip = ''
-        loss = ''
-        if line.find('statistics') != -1:
-            i = 4
-            while line[i] != 'p':
-                ip = ip + line[i]
-                i = i + 1
-
+        m = re.search(ip_pattern, line)
+        if m:
             line = f.readline()
-            
-            pos = line.find('packet loss')
-            if pos != -1:
-                pos = pos - 2
-                while line[pos] != ',':
-                    loss = loss + line[pos]
-                    pos = pos - 1
-            loss = loss[::-1]
-            if loss == ' 0.0%':
-                print ip + ":" + loss + ' Pass'
-                success += 1
-            elif loss != ' 100.0%':
-                print ip + ':' + loss + ' Loss'
-                loss  += 1
-            elif loss == ' 100.0%':
-                fail += 1
-
-    print "There are", success, " ping ok without any package loss"
-    print "There are", loss, " ping ok but with some package loss"
-    print "There are", fail, " ping fail"
+            percent = re.search(num_pattern, line)
+            if percent:
+                if percent.group(0) == '0.0%':
+                    print m.group(0) + ": OK"
+                elif percent.group(0) != '100.0%':
+                    print m.group(0) + ": " + percent.group(0) + " LOSS"
+                elif percent.group(0) == '100.0%':
+                    print m.group(0) + ": FAILED"
     f.close()
 
 def ping_list(ip_prefix, lower_hostid, upper_hostid):
-    print "In ping_list function\n"
     os_ping(ip_prefix, lower_hostid, upper_hostid)
     parse_logger()
-#	 print "Hello world in ping_list function"
-
-
 
 def check_param_valid(ip_prefix, lower_hostid, upper_hostid):
     if not lower_hostid.isdigit():
@@ -82,36 +62,25 @@ def check_param_valid(ip_prefix, lower_hostid, upper_hostid):
     if not upper_hostid.isdigit():
         print "The upper hostid is not valid\n"
         return False
-    
+    if int(lower_hostid) >= int(upper_hostid):
+        print "The upper hostid must be bigger than lower hostid\n"
+        return False
     #the ip net-id format:
     # xxx.xxx.xxx.
-    ip_num = ''
-    dot_count = 0
-    
-    for c in ip_prefix:
-        if c != '.':
-            ip_num += c
-        elif ip_num.isdigit():
-            dot_count += 1
-            if int(ip_num) > 0 and int(ip_num) <= 255:
-                ip_num = ''
-            else:
-                print "The ip prefix inputted is invalid\n"
-                print "The number should be between in 1~255\n"
-                return False
-
-    if dot_count != 3:
-        print "The ip prefix inputted is invalid\n"
-        print "it should be inputted as followed:\n"
-        print "xxx.xxx.xxx."
+    pattern = '([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){2}\.'
+    m = re.search(pattern, ip_prefix)
+    if not m or m.group(0) != ip_prefix:#short expression
+        print "The ip prefix is invalid\n"
         return False
     return True
 
 def help():
-    print "How to use this script:\n"
-    print "python ping_list.py ip_prefix lower_hostid upper_hostid\n"
-    print "For example:\n"
+    print "-----------------------------------------------"
+    print "How to use this script:"
+    print "python ping_list.py ip_prefix lower_hostid upper_hostid"
+    print "For example:"
     print "python ping_list.py 10.4.6. 1 10"
+    print "-----------------------------------------------\n"
 
 if __name__ == '__main__':
     #sys.argv[1] ---> ip_prefix
@@ -120,6 +89,7 @@ if __name__ == '__main__':
     if check_param_valid(sys.argv[1], sys.argv[2], sys.argv[3]):
         ping_list(sys.argv[1], sys.argv[2], sys.argv[3]) 
     else:
+        help()
         quit()
  #  print "script_name:", sys.argv[0]
  #  for i in range(1, len(sys.argv)):
